ac155a030317142eed8bfdbbb4889a7abda6faf3
lusiwei <lusiwei99@gmail.com>
2017-11-09 17:22:23 +0800

    :smile:

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-var bd=document.getElementById('bd');
-
+var bd=document.getElementById('bd')
+var gg=document.getElementById('gg')
+var input=document.getElementById('input')
 bd.onclick=function (xxx){
-    var input=document.getElementById('input')
     window.open('http://www.baidu.com/s?wd='+input.value,'_blank')
 }
-// gg.onclick=function(){
-
-// }
+gg.onclick=function(xxx){
+    window.open('http://www.google.com/search?q='+input.value,'_blank')
+}
