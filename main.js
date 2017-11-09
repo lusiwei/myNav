6fa88d020b557d4fccf8ded93d6d83f44d168113
lusiwei <lusiwei99@gmail.com>
2017-11-09 17:05:00 +0800

    增加搜索框

---
+++ main.js
@@ -0,0 +1,9 @@
+var bd=document.getElementById('bd');
+
+bd.onclick=function (xxx){
+    var input=document.getElementById('input')
+    window.open('http://www.baidu.com/s?wd='+input.value,'_blank')
+}
+// gg.onclick=function(){
+
+// }
